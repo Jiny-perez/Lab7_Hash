66548c81c9af2248b2b157682d96c40dd847144c
Jiny-perez <jinyperez55@gmail.com>
2025-12-05 08:37:16 -0600

    CLASS ENTRY FINAL

--- Hash/src/hash/Entry.java
+++ Hash/src/hash/Entry.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hash;
 
 /**
@@ -9,5 +5,15 @@ package hash;
  * @author marye
  */
 public class Entry {
-    
+
+    String username;
+    long posicion;
+
+    Entry siguiente;
+
+    public Entry(String username, long posicion) {
+        this.username = username;
+        this.posicion = posicion;
+        siguiente = null;
+    }
 }
